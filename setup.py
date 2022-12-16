828edd24b51b1bcec20eda8e778cf425343ecf10
Vishal kumar <Vishal.k@simplify3x.com>
2022-12-16 20:48:16 +0530

    changed version of vertica python and sqlalchemy

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 with open("README.rst", "r") as f:
     description = f.read()
 
-version_info = (0, 1, 0)
+version_info = (0, 1, 1)
 version = '.'.join(map(str, version_info))
 
 setup(
@@ -21,15 +21,15 @@ setup(
     ),
     install_requires=(
         'six >= 1.10.0',
-        'sqlalchemy >= 1.4.44',
-        'vertica-python>=1.1.1'
+        'sqlalchemy >= 1.1.11',
+        'vertica-python>=0.7.3'
     ),
     extras_require={
         'pyodbc': [
             'pyodbc>=4.0.16',
         ],
         'vertica-python': [
-            'vertica-python>=1.1.1',
+            'vertica-python>=0.7.3',
         ],
     },
     entry_points={
