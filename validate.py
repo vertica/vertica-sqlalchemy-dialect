c5727113f41290cc3dd8f60f4bb30eac0cbc7ff6
Narendra Prabhu <narendra.prabhu@microfocus.com>
2023-01-12 03:39:02 -0500

    Add license headers

--- validate.py
+++ validate.py
@@ -1,13 +1,31 @@
-import sqlalchemy as sa
-# vpyengine = sa.create_engine('vertica+vertica_python://dbadmin:abc123@localhost:5433/VMart?session_label=sqlalchemy&tlsmode=server')   
-# vpyconn = vpyengine.connect()
-# vpyres = vpyengine.dialect._get_server_version_info(vpyconn)
-# print(vpyres)
+# Copyright (c) 2018-2023 Micro Focus or one of its affiliates.
+# Copyright (c) 2017 StartApp Inc.
+# Copyright (c) 2015 Locus Energy
+# Copyright (c) 2013 James Casbon
+# Copyright (c) 2010 Bo Shi
 
-# vpyodbcengine = sa.create_engine('vertica+pyodbc://@verticadsn')
-# vpyodbcconn= vpyodbcengine.connect()
-# vpyodbcres = vpyodbcengine.dialect._get_server_version_info(vpyodbcconn)
-# print(vpyodbcres)
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sqlalchemy as sa
+vpyengine = sa.create_engine('vertica+vertica_python://dbadmin:abc123@localhost:5433/VMart?session_label=sqlalchemy&tlsmode=server')   
+vpyconn = vpyengine.connect()
+vpyres = vpyengine.dialect._get_server_version_info(vpyconn)
+print(vpyres)
+
+vpyodbcengine = sa.create_engine('vertica+pyodbc://@verticadsn')
+vpyodbcconn= vpyodbcengine.connect()
+vpyodbcres = vpyodbcengine.dialect._get_server_version_info(vpyodbcconn)
+print(vpyodbcres)
 
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Sequence, ForeignKey, PrimaryKeyConstraint, UniqueConstraint, sql
 engine = create_engine('vertica+vertica_python://dbadmin:abc123@localhost:5433/VMart?session_label=sqlalchemy', echo = True)
