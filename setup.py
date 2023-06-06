f44f1ff4c607ca3d9604b2b7593243fe97895b5f
vishal kumar <vishal.k@simplify3x.com>
2023-06-06 19:42:54 +0530

    changed sqlalchemy version

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     ),
     install_requires=(
         'six >= 1.10.0',
-        'sqlalchemy==1.3.24',
+        'sqlalchemy>=1.3.24,<=1.4.40',
         'vertica-python>=1.1.1'
     ),
     extras_require={
