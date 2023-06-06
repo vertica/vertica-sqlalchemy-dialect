4d9d5515a619f93de90f2d48ffe7e036cfbf0d48
vishal kumar <vishal.k@simplify3x.com>
2023-06-06 19:31:42 +0530

    test again

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     ),
     install_requires=(
         'six >= 1.10.0',
-        'sqlalchemy==1.4.48',
+        'sqlalchemy==1.3.24',
         'vertica-python>=1.1.1'
     ),
     extras_require={
