8b2c1e11b0bcaa55e737411c29ca1dbc32d6815b
vishal kumar <vishal.k@simplify3x.com>
2023-06-06 20:54:03 +0530

    checking 2.0 version

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     ),
     install_requires=(
         'six >= 1.10.0',
-        'sqlalchemy>=1.3.24,<=1.4.40',
+        'sqlalchemy<=2.0.0',
         'vertica-python>=1.1.1'
     ),
     extras_require={
